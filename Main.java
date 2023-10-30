1b837bf4e34642bb31f1b8129c79e8e9b5efee2d
Nurlisasafi <nurlisasafi@gmail.com>
2023-10-30 10:38:21 +0700

    first commit

--- Main.java
+++ Main.java
@@ -15,7 +15,7 @@ public class Main {
     public static void main(String[] args) {
         Main  obj = new Main();
         System.out.println("hasil kali dri 5 dan 7: " +obj.kali(5,7));
-        System.out.println(" hasil tambah: " +obj.tambah(5,7));
+        System.out.println("hasil tambah: " +obj.tambah(5,7));
         System.out.println("hasil kurang: " +obj.kurang(7,4));
         System.out.println("hasil pembagian: " +obj.bagi(6,3));
 
